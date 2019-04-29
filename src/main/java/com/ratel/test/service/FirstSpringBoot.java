49a10d90fb5245d2b698584ed6c31fdbd773dec2
sunwenhua <swh123456>
2019-04-29 10:11:53 +0800

    commit first springboot procedure

---
+++ src/main/java/com/ratel/test/service/FirstSpringBoot.java
@@ -0,0 +1,23 @@
+package com.ratel.test.service;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "testabc")
+@EnableAutoConfiguration
+public class FirstSpringBoot {
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public String test(){
+        return "abc";
+    }
+
+
+
+
+
+
+
+}
