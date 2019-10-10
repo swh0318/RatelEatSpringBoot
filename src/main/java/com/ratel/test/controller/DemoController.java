57e76f40b9da5265e28cba9cdae13f2c793802d7
sunwenhua <sunwenhua@haodf.com>
2019-10-10 15:25:43 +0800

    commit democontroller

--- src/main/java/com/ratel/test/controller/DemoController.java
+++ src/main/java/com/ratel/test/controller/DemoController.java
@@ -2,6 +2,17 @@ package com.ratel.test.controller;
 
 import org.springframework.stereotype.Controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class DemoController {
+
+    //测试Arrays.asList操作
+    public String testArraysAsList(){
+        String[] myArray = { "Apple", "Banana", "Orange" };
+        List<String> myList = Arrays.asList(myArray);
+        myList.toArray();
+        return "a";
+    }
 }
